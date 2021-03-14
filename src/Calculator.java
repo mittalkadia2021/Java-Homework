41d5575b0114274efa6aac3b4c7e6e6695293ea3
mittal <mittalkadia2021@gmail.com>
2021-03-14 21:15:36 +0000

    My first project

---
+++ src/Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 60;
+        int b = 30;
+        int total = a + b;
+        int subtraction = a - b;
+        int Multiplication= a*b;
+        int Division = a/b;
+
+
+        System.out.println("Addition of" + " " + a + " " + " &" + " " + b +" " +"= " +total);
+        System.out.println("Subtraction of" + " " + a + " " + "- " + b + " " + "=" + subtraction);
+        System.out.println("Multiplication of"+" "+ a + " *"+" "+ b +" ="+ Multiplication);
+        System.out.println("Division of"+" "+ a + " /"+" "+ b +" "+"="+Division);
+
+
+
+
+
+
+    }
+
+}
---
+++ src/PersonalDetails.java
@@ -0,0 +1,22 @@
+public class PersonalDetails {
+
+    public static void main(String[] args) {
+
+        String Name = "Meera Patel" ;
+        String Address = "16,London Road, SM4 8RD";
+        String Mobilenumber = "07967549876";
+        int Age = 37;
+        float SecurityCode = 12.56f;
+        boolean Eligibleforvote = true;
+        char Grade = 'b';
+
+        System.out.println("Name: "  + Name);
+        System.out.println("Address:" + Address);
+        System.out.println("Mobile no: "+Mobilenumber  );
+        System.out.println( "Age:" + Age );
+        System.out.println("Security code:"+ SecurityCode);
+        System.out.println("Eligible for vote="+ Eligibleforvote);
+        System.out.println("Grade="+ Grade);
+    }
+
+}
